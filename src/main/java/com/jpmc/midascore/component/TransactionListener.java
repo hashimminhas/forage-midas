a205de7736123661730b9345c5f3cef29d79066b
hashimali <hashim.ali@ut.ee>
2026-07-16 00:43:12 +0300

    task1 + 2

---
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -0,0 +1,18 @@
+package com.jpmc.midascore.component;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.jpmc.midascore.foundation.Transaction;
+
+@Component
+public class TransactionListener {
+
+    @KafkaListener(
+        topics = "${general.kafka-topic}",
+        groupId = "midas-consumer"
+    )
+    public void listen(Transaction transaction) {
+        System.out.println("Amount: " + transaction.getAmount());
+    }
+}
